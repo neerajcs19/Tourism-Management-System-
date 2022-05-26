cb3bad0a2a858f9f460237505083d5b71070a5fe
Neeraj Chaudhary <neerajcs19@icloud.com>
2022-05-26 09:31:06 +0530

    Update tms.sql

--- tms.sql
+++ tms.sql
@@ -1,33 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 11, 2021 at 06:38 AM
--- Server version: 10.3.15-MariaDB
--- PHP Version: 7.2.19
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `tms`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
+--Table structure
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `UserName` varchar(100) DEFAULT NULL,
